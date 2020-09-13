493bef5ab5eea09cdb9c9b1df9f61982b2eaae4e
Mauro Colella <mauro.ludovico.colella@gmail.com>
2020-09-13 20:08:29 +0800

    Add about me content

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,22 @@ import Layout from '../components/Layout'
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>About Me</h1>
+    <article>
+      <p>Hi, my name is Mauro Colella. I am a senior software developer with twenty years of activity in the IT industry. I develop great websites for great companies that offer great services.</p>
+      <p>I specialize in web applications and full-stack development, as well as user experience. From DevOps to responsive interfaces and tailored, well-optimized data visualizations, I am fluent with a broad range of technologies for the Internet.</p>
+      <br/>
+      <p>I have been successfully freelancing my services for the last ten years, with a strong focus on customer needs in both the business and the market.</p>
+      <br/>
+      <p>I am European, I trained in Switzerland, and I reside in Asia.</p>
+      <br/>
+      <p>When I am not in front of the computer, my interests range from being a dad to learning about art, motion graphics, architecture and much more.</p>
+      <p>For me, command of information technology is a means to empower people to share and access information. It is first and foremost a means to enable and facilitate intercommunication.</p>
+      <br/>
+      <p>I'd like to welcome  you to my abode. I hope you enjoy your visit, and I am looking forward to developing for you a platform that enables you to truly achieve and exceed every one of your professional targets and ambitions.</p>
+      <p>It sounds simple but this is what I do.</p>
+      <br/>
+      <p>I make it simple. For you.</p>
+    </article>
   </Layout>
 )
 
