27f4331cbdd182372520671760bf24324b2a3540
Mauro Colella <mauro.ludovico.colella@gmail.com>
2020-10-01 15:22:56 +0800

    Update copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>About Me</h1>
     <article>
-      <p>Hi, my name is Mauro Colella. I am a senior software developer with twenty years of activity in the IT industry. I develop great websites for great companies that offer great services.</p>
+      <p>Hi, my name is Mauro Colella. I am a senior software developer with twenty years of activity in the IT industry. I develop tailored solutions for great companies that offer great services.</p>
       <p>I specialize in web applications and full-stack development, as well as user experience. From DevOps to responsive interfaces and tailored, well-optimized data visualizations, I am fluent with a broad range of technologies for the Internet.</p>
       <br/>
       <p>I have been successfully freelancing my services for the last ten years, with a strong focus on customer needs in both the business and the market.</p>
