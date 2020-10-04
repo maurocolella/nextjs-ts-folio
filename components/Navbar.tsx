91fb9d01b6be087f322d78e99a4461fcb6ece0b9
Mauro Colella <mauro.ludovico.colella@gmail.com>
2020-10-04 15:33:21 +0800

    Add progress indicator

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import { PureComponent, RefObject, createRef } from 'react';
 import styles from './styles/Navbar.module.scss';
 
 type State = {
+  currentOffset: number
   navbarSticky: boolean
   startOffset: number
 };
@@ -16,6 +17,7 @@ export class Navbar extends PureComponent<{}, State> {
 
     this.navbarRef = createRef();
     this.state = {
+      currentOffset: 0,
       navbarSticky: false,
       startOffset: 0,
     };
@@ -24,12 +26,12 @@ export class Navbar extends PureComponent<{}, State> {
   componentDidMount() {
     this.setState({
       startOffset: this.navbarRef.current!.offsetTop,
-    })
+    });
     this.checkScrollOffset();
   }
 
   checkScrollOffset = () => {
-    const { navbarSticky, startOffset } = this.state;
+    const { currentOffset, navbarSticky, startOffset } = this.state;
 
     const currentState = window.pageYOffset > startOffset;
 
@@ -39,11 +41,25 @@ export class Navbar extends PureComponent<{}, State> {
       });
     }
 
+    const body = document.body;
+    const htmlEl = document.documentElement;
+
+    const documentHeight = Math.max( body.scrollHeight, body.offsetHeight,
+                     htmlEl.clientHeight, htmlEl.scrollHeight, htmlEl.offsetHeight ) - window.innerHeight;
+
+    const scrollOffset = Number(((window.pageYOffset * 100) / documentHeight).toFixed(0));
+
+    if (scrollOffset !== currentOffset) {
+      this.setState({
+        currentOffset: (currentOffset + scrollOffset) / 2,
+      });
+    }
+
     window.requestAnimationFrame(this.checkScrollOffset);
   }
 
   render() {
-    const { navbarSticky } = this.state;
+    const { currentOffset, navbarSticky } = this.state;
 
     const deco = [styles.navbar, navbarSticky && styles['navbar--sticky']].filter(Boolean).join(' ');
 
@@ -52,18 +68,21 @@ export class Navbar extends PureComponent<{}, State> {
         className={deco}
         ref={this.navbarRef}
       >
-        <ul className={styles.navbar__nav}>
-          <li className={styles.navbar__item}>
-            <Link href="/">
-              <a className={styles.navbar__link}>About</a>
-            </Link>{' '}
-          </li>
-          <li className={styles.navbar__item}>
-            <Link href="/resume">
-              <a className={styles.navbar__link}>Resume</a>
-            </Link>
-          </li>
-        </ul>
+        <div className={styles.navbar__wrapper}>
+          <div className={styles.navbar__progress} style={{ width: `${currentOffset}%` }} />
+          <ul className={styles.navbar__nav}>
+            <li className={styles.navbar__item}>
+              <Link href="/">
+                <a className={styles.navbar__link}>About</a>
+              </Link>{' '}
+            </li>
+            <li className={styles.navbar__item}>
+              <Link href="/resume">
+                <a className={styles.navbar__link}>Resume</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     );
   }
